e79868c63d65e9127152b5ad3cb1f9589d2c25e2
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-03 12:00:11 -0500

    starter for react-redux

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+import BookList from '../containers/book-list';
+
+
+export default class App extends Component {
+  render() {
+    return (
+    	<div>
+    		<h1>wat the helll yo</h1>	
+      		<BookList />
+      	</div>
+      );
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+		{title: 'Javascript : the Good parts '},
+		{title: 'Harry Potter'},
+		{title: 'The Dark Tower'},
+		{title: 'Eloguent Ruby'}
+	]
+}
